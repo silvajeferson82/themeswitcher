d1be311084a57a2ad49ea9aa7e8b45e5ccf20d8b
silvajeferson82 <jefersonsilva.contato@gmail.com>
2021-04-17 00:36:45 -0300

    Configuração de localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import React, { useState } from 'react';
-import { ThemeProvider } from 'styled-components';
+import React from 'react';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+import usePersistedState from './utils/usePersistedState';
+
 import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+
 import GlobalStyle from './styles/global'
 import Header from './components/header';
 
-
-
 function App() {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme',light);
 
-  const [theme, setTheme] = useState(light);
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);  
+  };
 
   return (
-    <ThemeProvider theme={light}>
-    <div className="App">
+    <ThemeProvider theme={theme}>
+    <div>
       <GlobalStyle/>
-      <Header/>
+      <Header toggleTheme={toggleTheme}/>
     </div>
     </ThemeProvider>
   );
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,25 +4,26 @@ import { ThemeContext } from 'styled-components';
 import { shade } from 'polished';
 
 import { Container } from './style';
+interface Props{
+   toggleTheme(): void;
+}
 
-
-const Header: React.FC = () =>{
-
-   const { colors } = useContext(ThemeContext);
+const Header: React.FC<Props> = ({toggleTheme}) =>{
+   const { colors, title } = useContext(ThemeContext);
 
    return (
       <Container>
          Hello Word!
          
          <Switch
-            onChange={() => {}}
-            checked={false}
+            onChange={toggleTheme}
+            checked={title === 'dark'}
             checkedIcon={false}
             uncheckedIcon={false}
             height={10}
             width={40}
             handleDiameter={20}
-            offColor={shade(0.1, colors.primary)}
+            offColor={shade(0.15, colors.primary)}
             onColor={colors.secondary}
          />
       </Container>
